eebc18bb8459bfa38f595f9b38c8441308fe3403
Aparna Raj <aparnaraj448@gmail.com>
2022-10-25 11:17:50 +0530

    updating directory page with directory component and category page with category component

---
+++ src/components/directory/directory.component.jsx
@@ -0,0 +1,46 @@
+import React from 'react'
+import CategoryItem from '../category-item/category-item.component'
+import './directory.styles.scss'
+
+const Directory = () => {
+
+    const categories=[
+        {
+          "id": 1,
+          "title": "HATS",
+          "imageUrl": "https://i.ibb.co/cvpntL1/hats.png"
+        },
+        {
+          "id": 2,
+          "title": "JACKETS",
+          "imageUrl": "https://i.ibb.co/px2tCc3/jackets.png"
+        },
+        {
+          "id": 3,
+          "title": "SNEAKERS",
+          "imageUrl": "https://i.ibb.co/0jqHpnp/sneakers.png"
+        },
+        {
+          "id": 4,
+          "title": "WOMENS",
+          "imageUrl": "https://i.ibb.co/GCCdy8t/womens.png"
+        },
+        {
+          "id": 5,
+          "title": "MENS",
+          "imageUrl": "https://i.ibb.co/R70vBrQ/men.png"
+        }
+      ]
+  return (
+    <div className='categories-container'>
+    {
+      categories.map((category)=>(
+         <CategoryItem key={category.id} category={category}/>
+      ))
+    }
+    
+</div>
+  )
+}
+
+export default Directory
---
+++ src/homepage.component.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+import Directory from './components/directory/directory.component'
+
+const HomePage = () => {
+
+  return (
+
+  <Directory/>
+  
+  )
+}
+
+export default HomePage
